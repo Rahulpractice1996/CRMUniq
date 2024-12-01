250d6447bad5d6a1b4193f680dff5f82b9b1749d
Rahulpractice1996 <2smartpractice@gmail.com>
2024-12-01 18:29:47 +0530

    latest

--- src/main/java/com/org/CRMUniq/viewController/ManagerViewController.java
+++ src/main/java/com/org/CRMUniq/viewController/ManagerViewController.java
@@ -16,67 +16,71 @@ import com.org.CRMUniq.service.ManagerService;
 
 import jakarta.servlet.http.HttpSession;
 
-
 @Controller
 public class ManagerViewController {
-	
+
 	@Autowired
 	LeadService leadService;
 	@Autowired
 	AllUserService allUserSevice;
 	@Autowired
 	ManagerService managerservice;
-	
+
 	@GetMapping("/Managerhome")
-	public String bless(Model model,HttpSession session) {
-		AllUsers SessionUser  =(AllUsers) session.getAttribute("User");
+	public String bless(Model model, HttpSession session) {
+		AllUsers SessionUser = (AllUsers) session.getAttribute("User");
 		model.addAttribute("manager", SessionUser);
-		return"Managerhome";
+		return "Managerhome";
 	}
-	
+
 	@GetMapping("/AllLeadsManagerMysmartView")
-	public String mysmartView(Model model,HttpSession session) {
-		
-		AllUsers SessionUser  =(AllUsers) session.getAttribute("User");
-		//System.out.println(">>>>>>>>>>>>>"+SessionUser);
-		
-		List<Long> SalesUsersId=managerservice.getByID(SessionUser.getEID()).getSalesUser().stream().map(s->s.getSUID())
-				.collect(Collectors.toList());
+	public String mysmartView(Model model, HttpSession session) {
+
+		AllUsers SessionUser = (AllUsers) session.getAttribute("User");
+		// System.out.println(">>>>>>>>>>>>>"+SessionUser);
+
+		List<Long> SalesUsersId = managerservice.getByID(SessionUser.getEID()).getSalesUser().stream()
+				.map(s -> s.getSUID()).collect(Collectors.toList());
 
 		List<Leads> leads = null;
-		if(SessionUser!=null && SessionUser.getRole().equalsIgnoreCase("Manager"))
-		{
+		if (SessionUser != null && SessionUser.getRole().equalsIgnoreCase("Manager")) {
 			// this block for managers MyTaskview
-			leads = leadService.getAllLeads().stream()
-					.filter(l->SalesUsersId.contains(l.getLeadOwnerId()) 
-					&& !l.getLeadStatus().equalsIgnoreCase("Enrolled"))
-					.filter(l->!l.getLeadStatus().equalsIgnoreCase("rejected"))
-					.collect(Collectors.toList()); 	
+			leads = leadService.getAllLeads().stream().filter(
+					l -> SalesUsersId.contains(l.getLeadOwnerId()) && !l.getLeadStatus().equalsIgnoreCase("Enrolled"))
+					.filter(l -> !l.getLeadStatus().equalsIgnoreCase("rejected")).collect(Collectors.toList());
 		}
-		
+
 		model.addAttribute("leads", leads);
 		List<AllUsers> Managers = allUserSevice.GetByRole("Manager");
 		List<AllUsers> SalesUsers = allUserSevice.GetByRole("SalesUser");
 		model.addAttribute("Managers", Managers);
 		model.addAttribute("SalesUsers", SalesUsers);
-		
+
 		return "My Smart View";
-	} 
-	
-	@GetMapping("/leadsBucket") 
+	}
+
+	@GetMapping("/leadsBucket")
 	private String leads(Model model, HttpSession session) {
-		AllUsers SessionUser  =(AllUsers) session.getAttribute("User");
-		
-		List<Leads> leads = leadService.getAllLeads().stream().filter(l->l.getLeadOwnerRole().equalsIgnoreCase("Manager")
-				&& l.getLeadOwnerId()==SessionUser.getEID()
-				&&!l.getLeadStatus().equalsIgnoreCase("rejected")
-				&&!l.getLeadStatus().equalsIgnoreCase("Enrolled"))
-				.collect(Collectors.toList()); 		
+		AllUsers SessionUser = (AllUsers) session.getAttribute("User");
+
+		List<Leads> leads = leadService.getAllLeads().stream().filter(l -> {
+			if (l.getLeadOwnerRole() != null 
+					&& l.getLeadOwnerRole().equalsIgnoreCase("Manager")
+					&& l.getLeadOwnerId() == SessionUser.getEID() && !l.getLeadStatus().equalsIgnoreCase("rejected")
+					&& !l.getLeadStatus().equalsIgnoreCase("Enrolled")) {
+				return true;
+			} else {
+				return false;
+			}
+
+		})
+
+				.collect(Collectors.toList());
 		model.addAttribute("leads", leads);
-		
-		List<Long> SalesUsersId=managerservice.getByID(SessionUser.getEID()).getSalesUser().stream().map(s->s.getSUID())
-				.collect(Collectors.toList());		
-	
+
+		List<Long> SalesUsersId = managerservice.getByID(SessionUser.getEID()).getSalesUser().stream()
+				.map(s -> s.getSUID()).collect(Collectors.toList());
+
 		List<AllUsers> SalesUsers = allUserSevice.getById(SalesUsersId);
 		model.addAttribute("Managers", SessionUser);
 		model.addAttribute("SalesUsers", SalesUsers);
